9e94cf4c8cc76329a233f790244cdef830e0735f
Karla Jara <jara.karla.43@gmail.com>
2021-05-26 15:55:40 -0500

    added code for WebServlet

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -10,12 +10,22 @@ import java.io.PrintWriter;
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
 public class HelloWorldServlet extends HttpServlet {
 
-    @Override
+        private int viewCount;
+
+        public void countViews(){
+            viewCount++;
+        }
+
+        @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            // response content type
         response.setContentType("text/html");
+            // runs when servlet is triggered and increments viewCount
+        countViews();
         PrintWriter out = response.getWriter();
         String name = "Karla";
         out.println("<h1>Hello, " + name + " !</h1>");
+        String title = "Total Number of Hits";
+        out.println(    "<h1 align = \"center\">" + title + "</h1>\n" + "<h2 align = \"center\">" + viewCount + "</h2>\n");
     }
-
 }
