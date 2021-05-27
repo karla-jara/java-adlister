77ebce041272d680c4c0d870011a80351b7787c4
Karla Jara <jara.karla.43@gmail.com>
2021-05-27 15:58:08 -0500

    added files for jsp exercise

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        PrintWriter out = response.getWriter();
+        out.println("");
+        String username;
+        String password;
+
+    }
+
+}
