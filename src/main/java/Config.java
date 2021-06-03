be154b0a0396f1f64149be234b9e65328cf93141
Karla Jara <jara.karla.43@gmail.com>
2021-06-03 05:29:00 -0500

    added code for sessions-and-cookies-exercise

---
+++ src/main/java/Config.java
@@ -0,0 +1,19 @@
+public class Config {
+    private String url = "jdbc:mysql://localhost/adlister_db?serverTimezone=UTC&useSSL=false";
+    private String username = "user";
+    private String password = "password";
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+
+}
--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
@@ -14,13 +15,18 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+        HttpSession session = request.getSession();
 
+        if ((boolean) session.getAttribute("user")) {
+            response.sendRedirect("profile");
 
-        if (username.equals("admin") && password.equals("password")) {
+        } else if (username.equals("admin") && password.equals("password")) {
             request.getSession().setAttribute("isAdmin", true);
             request.setAttribute("admin", username);
             request.setAttribute("password", password);
-            response.sendRedirect("/profile");
+            session.setAttribute("user", true);
+            response.sendRedirect("profile");
+
         } else {
             response.sendRedirect("/login");
         }
